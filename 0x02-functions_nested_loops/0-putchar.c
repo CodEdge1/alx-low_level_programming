0e4aef235a5aadc8826e7f36c33bb900f8f3bed7
Lexz1 <alexandraogbe@gmail.com>
2023-06-20 15:25:15 -0700

    my first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main entry point
+ *
+ * returns 0
+**/ 
+
+int main(void)
+{
+	puts("_putchar");
+	return(0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This file olds the discription to all the files in this directory
